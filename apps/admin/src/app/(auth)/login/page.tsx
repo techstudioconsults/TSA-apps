7db5a6872f7dca566530e78f98a255deac40c297
i19n <kinxly@gmail.com>
2026-01-16 13:40:02 +0100

    fix: resolve all ui issues in bith we and admin

--- apps/admin/src/app/(auth)/login/page.tsx
+++ apps/admin/src/app/(auth)/login/page.tsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
 import LoginForm from "./views/LoginForm";
 
 const Login = () => {
   return (
-    <>
+    <Suspense
+      fallback={
+        <div className="flex min-h-screen items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
       <LoginForm />
-    </>
+    </Suspense>
   );
 };
 export default Login;
